a816df4847620410c4ba84f38a761f1a628a61aa
Tobias Winchen <tobias.winchen@rwth-aachen.de>
2020-01-20 13:53:01 +0100

    Pass additional flags to anisble in quickbuild

--- quick_build_role.sh
+++ quick_build_role.sh
@@ -6,19 +6,20 @@ Built ROLE on the development server
 Usage: $0 <ROLE> [ansible-playbook options]
 
 Examples:
-  $0 localhost eddbase
-  $0 edd01 edd_master_controller -vv
+  $0 eddbase
+  $0 edd_master_controller -vv
 HELP
   exit
 fi
 
 ROLE=$1
+shift 
 
 echo "Building role \"$ROLE\" ..."
 
 export ANSIBLE_ROLES_PATH="$(pwd)/roles"
 export ANSIBLE_RETRY_FILES_ENABLED="False"
-ansible-playbook -i site.yml --tags=build /dev/stdin <<END
+ansible-playbook "$@" -i site.yml --tags=build /dev/stdin <<END
 ---
 - hosts: dev_server
   roles:
