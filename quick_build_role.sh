42ef85092f05b933c2472ec2220f9be7155e37e3
Tobias Winchen <tobias.winchen@rwth-aachen.de>
2020-02-18 14:31:36 +0100

    Specify base path for quickbuild script

--- quick_build_role.sh
+++ quick_build_role.sh
@@ -19,9 +19,16 @@ echo "Building role \"$ROLE\" ..."
 
 export ANSIBLE_ROLES_PATH="$(pwd)/roles"
 export ANSIBLE_RETRY_FILES_ENABLED="False"
-ansible-playbook "$@" -i site.yml --tags=build /dev/stdin <<END
+ansible-playbook "$@" -i site.yml -e base_path=`pwd` --tags=build /dev/stdin <<END
 ---
 - hosts: dev_server
+  gather_facts: no
+  tasks:
+  - name: Print path variable
+    debug:
+      var: base_path
+    tags: build
+
   roles:
     - $ROLE
 END
